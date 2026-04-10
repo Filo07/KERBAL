f0d44f4a98725007db80341aa58ecff0e221056f
Filo <filipingback@gmail.com>
2026-04-10 12:31:58 +0200

    v1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <img src="bilder/bg.png" alt="Space!" class="bg">
+    <section class="ui">
+        <img src="bilder/logo.png" alt="Logo" class="logo">
+        
+
+    </section>
+</body>
+</html>
