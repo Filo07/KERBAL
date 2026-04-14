dc2f009db39fbcc788e8d8a6b5bc734a33cde6c2
Filo <filipingback@gmail.com>
2026-04-15 00:01:11 +0200

    wow

--- index.php
+++ index.php
@@ -4,14 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <img src="bilder/bg.png" alt="Space!" class="bg">
-    <section class="ui">
-        <img src="bilder/logo.png" alt="Logo" class="logo">
-        
+    <header>
+        <img src="bilder/logo-removebg-preview.png" alt="Logo">
 
-    </section>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
